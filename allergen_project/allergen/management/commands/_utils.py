7ff6035b223f6ae36144e1e93f462c2a59b29869
Ducluzeaud David <david.ducluzeaud@live.fr>
2018-07-12 21:55:11 +0200

    better translation method and factorization

--- allergen_project/allergen/management/commands/_utils.py
+++ allergen_project/allergen/management/commands/_utils.py
@@ -1,36 +1,14 @@
-import requests
+from typing import List
 
 import pandas as pd
+import requests
 from googletrans import Translator
+import string
+
+from .languages import LANGCODES
 
 
-def count_pages():
-    """
-    Count pages for a category
-    :return: page_number, integer
-    """
-    url = 'https://fr.openfoodfacts.org/categorie/'
-    url += 'Aliments et boissons à base de végétaux.json'
-    json_file = requests.get(url).json()
-    page_number = json_file['count'] // 20
-    if json_file['count'] % 20 != 0:
-        page_number += 1
-    return page_number
-
-
-def generate_products(page):
-    """
-    Query the api by category and by page
-    :param page: integer that fetch the api at the page number
-    :return products['products']: json data containing all informations
-    """
-    url = 'https://fr.openfoodfacts.org/categorie/'
-    url += 'Aliments et boissons à base de végétaux/' + str(page) + '.json'
-    products = requests.get(url).json()
-    return products['products']
-
-
-def make_translation(word):
+def make_translation(word: str) -> str:
     """
     Translate a word using GoogleTrans library
     The language might be indicate in the 2 first character
@@ -46,29 +24,40 @@ def make_translation(word):
     elif word == 'en:plant-based-spreads':
         return 'Pâte à tartiner végétal'
     else:
-        translator = Translator()
-        # take the language indication
-        language = get_language(word)
-        # remove language indicator
-        word = slice_language(word)
-        # translate word to french
-        return translator.translate(word, src=language, dest='fr').text
+        try:
+            translator = Translator()
+            # take the language indication
+            language = get_language(word)
+            # remove language indicator
+            word = slice_language(word)
+            # translate word to french
+            word = translator.translate(word, src=language, dest='fr').text
+            # remove any type of punctuation
+            trans = str.maketrans(' ', ' ', string.punctuation)
+            return word.translate(trans)
+        except ValueError:
+            # if invalid language source return word
+            return word
 
 
-def detect_lang(word):
+def detect_lang(word: str) -> str:
+    """Detect language with googletrans"""
     translator = Translator()
     # detect language
     tr = translator.detect(word)
-    return tr.lang
+    for l in LANGCODES.values():
+        if tr.lang == l:
+            return tr.lang
+    return 'unkonwn'
 
 
-def slice_language(word):
+def slice_language(word: str) -> str:
     if len(word) > 3 and word[2] == ':':
         word = word[3:]
     return word
 
 
-def get_language(word):
+def get_language(word: str) -> str:
     """
     Return the language of the word if its has a language indicator.
     If its has not googletrans library will detect its language
@@ -88,44 +77,121 @@ def get_language(word):
     return lang
 
 
-def generate_dataframe():
-    nb_pages = count_pages() + 1
-    for page in range(1, nb_pages):
-        print(page)
-        products = generate_products(page)
-        df = pd.DataFrame(products)
-        # Select columns
-        try:
-            df = df[[
-                'product_name',
-                'code',  # barcode
-                'image_small_url',  # url of the image of the product
-                'url',  # openfoodfacts url
-                'nutrition_grades',
-                'quantity',  # provide the measurement unit
-                'categories_hierarchy',   # list all categories
-                'additives_tags',  # list all additives
-                'allergens_hierarchy',  # list all allergens
-                'ingredients',  # list all ingredients
-                'nutrient_levels',  # sugar , salt, fat, saturated-fat level
-                'nutriments',  # list all nutriments
-                'traces_hierarchy',  # list all traces of allergens
-            ]]
-            # Remove row of the dataframe if this columns contains null value
-            df = df.dropna(subset=[
-                'product_name',
-                'code',
-                'nutrition_grades',
-                'categories_hierarchy', 
-                'ingredients',
-                'url', 
-                'image_small_url',
-            ])
-            yield df
-        except KeyError:
-            pass
+def normalize_value(value: float, unit: str) -> float:
+    """Convert all value to mg, mL or kcal."""
+    try:
+        value = float(value)
+    # if value can't be cast the value must be incorrect
+    # return 0
+    except ValueError:
+        return 0.0
+
+    if unit.lower() == 'mg' or unit.lower() == 'kcal'or unit.lower() == 'ml':
+        return value
+    elif unit.lower() == 'kg':
+        value = value * 1000000
+    elif unit.lower() == 'g':
+        value = value * 1000
+    elif unit.lower() == 'µg':
+        value = value / 1000
+    elif unit.lower() == 'kj':
+        value = value * 239.005
+    elif unit.lower() == 'L':
+        value = value * 1000
+    elif unit == 'µL':
+        value = value / 1000
+    else:
+        value = 0.0
+    return value
 
 
-def concat_dataframe():
-    generator = generate_dataframe()
-    return pd.concat([x for x in generator], ignore_index=True)
+class Singleton(type):
+    """ Create a singleton"""
+    _instances = {}
+
+    def __call__(cls, *args, **kwargs):
+        if cls not in cls._instances:
+            cls._instances[cls] = super(
+                Singleton, cls).__call__(*args, **kwargs)
+        return cls._instances[cls]
+
+
+class ProductDataFrame(metaclass=Singleton):
+    """
+    Generate a dataframe concatenating all pages founds related to
+    the category on openfoodfacts.
+    """
+
+    def __init__(self, category: str):
+        self.category = category
+
+    def _count_pages(self, category: str) -> int:
+        """
+        Count pages for a category
+        :return: page_number, integer
+        """
+        url = 'https://fr.openfoodfacts.org/categorie/'
+        url += self.category + '.json'
+        json_file = requests.get(url).json()
+        page_number = json_file['count'] // 20
+        if json_file['count'] % 20 != 0:
+            page_number += 1
+        return page_number
+
+    def _generate_products(self, page: int) -> List:
+        """
+        Query the api by category and by page
+        :param page: integer that fetch the api at the page number
+        :return products['products']: json data containing all informations
+        """
+        url = 'https://fr.openfoodfacts.org/categorie/'
+        url += self.category + '/' + str(page) + '.json'
+        products = requests.get(url).json()
+        return products['products']
+
+    def _generate_dataframe(self):
+        """
+        Query openfoodfacts and concatenate the dataframe
+        for every page in the category. The dataframe select
+        column and remove empty row if some values are missing
+        in the column.
+        """
+        nb_pages = self._count_pages(self.category) + 1
+        for page in range(1, nb_pages):
+            print(page)
+            products = self._generate_products(page)
+            df = pd.DataFrame(products)
+            # Select columns
+            try:
+                df = df[[
+                    'product_name',
+                    'code',  # barcode
+                    'image_small_url',  # url of the image of the product
+                    'url',  # openfoodfacts url
+                    'nutrition_grades',
+                    'quantity',  # provide the measurement unit
+                    'categories_hierarchy',   # list all categories
+                    'additives_tags',  # list all additives
+                    'allergens_hierarchy',  # list all allergens
+                    'ingredients',  # list all ingredients
+                    'nutriments',  # list all nutriments
+                    'traces_hierarchy',  # list all traces of allergens
+                ]]
+                # Remove row of the dataframe
+                # if this columns contains null value
+                df = df.dropna(subset=[
+                    'product_name',
+                    'code',
+                    'nutrition_grades',
+                    'categories_hierarchy',
+                    'ingredients',
+                    'url',
+                    'image_small_url',
+                ])
+                yield df
+            except KeyError:
+                pass
+
+    def concat_dataframe(self):
+        generator = self._generate_dataframe()
+        return pd.concat([x for x in generator], ignore_index=True)
