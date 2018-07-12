7e7daea7ba36f51ed533a7b77a32737f2c961fac
Ducluzeaud David <david.ducluzeaud@live.fr>
2018-07-12 23:02:24 +0200

    add api

--- allergen_project/allergen_project/settings.py
+++ allergen_project/allergen_project/settings.py
@@ -38,7 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'debug_toolbar',
+    'django-rest',
     'allergen',
+    'api.apps.ApiConfigs'
 ]
 
 MIDDLEWARE = [
