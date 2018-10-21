86539b8c78cddc21b36bddd70f36386062383c48
sneakyPeat <david.ducluzeaud@live.fr>
2018-10-21 19:05:43 +0200

    add id for nutriment

--- api/serializers.py
+++ api/serializers.py
@@ -38,7 +38,7 @@ class TraceSerializer(serializers.ModelSerializer):
 class NutrimentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Nutriment
-        fields = ('nutriment_name', 'description', 'daily_quantity_m', 'daily_quantity_f', )
+        fields = ('id', 'nutriment_name', 'description', 'image', 'daily_quantity_m', 'daily_quantity_f', )
 
 
 class NutrimentComposeProductSerializer(serializers.HyperlinkedModelSerializer):
