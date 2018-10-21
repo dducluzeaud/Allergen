8da83ff97c3e7a78bc863f58abe51a9c7766f3ae
sneakyPeat <david.ducluzeaud@live.fr>
2018-10-21 19:12:35 +0200

    app vue

---
+++ vueapp/src/App.vue
@@ -0,0 +1,44 @@
+<template>
+  <div id="app">
+    <section class="hero is-fullheight is-default is-bold">
+      <TheNavbar/>
+      <div class="container">
+        <router-view/>
+      </div>
+      <TheFooter/>
+    </section>
+  </div>
+</template>
+
+<script>
+  import TheNavbar from '@/components/TheNavbar'
+  import TheFooter from '@/components/TheFooter'
+  export default {
+    components: {
+      TheNavbar,
+      TheFooter
+    }
+  }
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
+}
+</style>
