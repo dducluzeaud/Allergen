ef6c68eaff19e231469e69857143805ca6d97194
sneakyPeat <david.ducluzeaud@live.fr>
2018-10-21 19:09:54 +0200

    footer component

---
+++ vueapp/src/components/TheFooter.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="hero-foot">
+      <div class="container">
+        <div class="tabs is-centered">
+            <ul>
+                <li>
+                  <router-link :to="{name: 'About'}">Mentions légales</router-link>
+                </li>
+            </ul>
+        </div>
+      </div>
+    </div>
+</template>
