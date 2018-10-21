d611e97dc45f94c2de067ea8f0be64d9eea0c314
sneakyPeat <david.ducluzeaud@live.fr>
2018-10-21 19:11:45 +0200

    home page

---
+++ vueapp/src/views/Home.vue
@@ -0,0 +1,43 @@
+<template>
+    <div class="hero-body">
+        <div class="container has-text-centered">
+            <div class="columns is-vcentered">
+                <div class="column is-5">
+                    <figure class="image is-4by3">
+                        <img src="https://picsum.photos/800/600/?random" alt="Description">
+                    </figure>
+                </div>
+                <div class="column is-6 is-offset-1">
+                    <h1 class="title is-2">
+                        Superhero Scaffolding
+                    </h1>
+                    <h2 class="subtitle is-4">
+                        Let this cover page describe a product or service.
+                    </h2>
+                    <br>
+                    <p class="has-text-centered">
+                        <a class="button is-medium is-info is-outlined">Learn more</a>
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+
+<style type="text/css">
+    html,
+    body {
+        font-family: 'Open Sans';
+        }
+    img {
+        padding: 5px;
+        border: 1px solid #ccc;
+        }
+</style>
