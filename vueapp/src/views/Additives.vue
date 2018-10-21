c7674a87f915641ec47c92ba9dab93b84c150562
sneakyPeat <david.ducluzeaud@live.fr>
2018-10-21 19:11:31 +0200

    component to list additive

---
+++ vueapp/src/views/Additives.vue
@@ -0,0 +1,122 @@
+<template>
+    <section id="additives" class="section">
+         <b-table
+            :data="additives"
+            
+            paginated
+            backend-pagination
+            :total="total"
+            :per-page="perPage"
+            @page-change="onPageChange"
+            
+            backend-sorting
+            :default-sort-direction="defaultSortOrder"
+            :default-sort="[sortField, sortOrder]"
+            @sort="onSort">
+
+            <template slot-scope="props">
+                <b-table-column class="has-text-centered" field="risk" label="Risque" centered sortable>
+                    {{ emojize(props.row.risk) }}
+                </b-table-column>
+
+                <b-table-column class="has-text-centered" field="additive_name" label="Additif" centered sortable >
+                    {{ props.row.additive_name |capitalize }}
+                </b-table-column>
+
+                <b-table-column field="description" label="Description">
+                    {{ props.row.description }}
+                </b-table-column>
+
+            </template>
+        </b-table>
+            <ul>
+                <li>✅ Sans risque</li>
+                <li>⚠️ Peu présenté des risques</li>
+                <li>❓ Douteux</li>
+                <li>⁉️ Présente des risques</li>
+                <li>⛔️ A éviter</li>
+            </ul>
+    </section>
+</template>
+
+<script>
+    import {
+        APIServiceAdditives
+    } from '../APIService';
+
+    const APIAdditives = new APIServiceAdditives();
+
+    export default {
+        data() {
+            return {
+                additives: [],
+                total: 0,
+                nextPage: '',
+                previousPage: '',
+                sortField: 'additive_name',
+                sortOrder: '-',
+                defaultSortOrder: '',
+                page: 1,
+                perPage: 20
+            };
+        },
+        created () {
+            // Fethc the data the view is created
+            // and the data is already being observed
+            this.getAdditives()
+        },
+        watch: {
+            // call again the method if the route changes
+            '$route': 'getAdditives'
+        },
+        methods: {
+            getAdditives() {
+                const params = [
+                    `ordering=${this.sortOrder}${this.sortField}`,
+                    `offset=${(this.page - 1) * 20}`
+                ].join('&')
+                this.loading = true
+                APIAdditives.getAdditives(params).then((data) => {
+                    this.additives = data.results
+                    this.total = data.count
+                    this.nextPage = data.next
+                    this.previousPage = data.previous
+                }).catch((error) => {
+                        this.data = []
+                        this.total = 0
+                        this.loading = false
+                        throw error
+                });
+                this.loading = false
+            },
+            /*
+             * Handle page-change event
+             */
+            onPageChange(page) {
+                this.page = page
+                this.getAdditives()
+            },
+            /*
+             * Handle sort event
+             */
+            onSort(field, order) {
+                this.sortField = field
+                this.sortOrder = (order === 'desc') ? "-" : " "
+                this.getAdditives()
+            },
+            emojize(risk) {
+                if (risk === 0) {
+                    return '✅'
+                } else if (risk === 1) {
+                    return '⚠️'
+                } else if (risk === 2) {
+                    return '❓'
+                } else if (risk === 3) {
+                    return '⁉️'
+                } else if (risk === 4) {
+                    return '⛔️'
+                }
+            }
+        }
+    }
+</script>
