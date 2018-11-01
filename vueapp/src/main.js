23628fe33aa36fa904da2c4b7c233ac6318e5ad4
sneakyPeat <david.ducluzeaud@live.fr>
2018-11-01 11:17:57 +0100

    add @

--- vueapp/src/main.js
+++ vueapp/src/main.js
@@ -1,6 +1,9 @@
 import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
+import App from '@/App.vue'
+import router from '@/router'
+import store from '@/store'
+
+
 import Buefy from 'buefy'
 import 'buefy/dist/buefy.css'
 
@@ -15,5 +18,6 @@ Vue.filter('capitalize', function (value) {
 
 new Vue({
   router,
+  store, 
   render: h => h(App)
 }).$mount('#app')
