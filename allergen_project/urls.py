df646ae6d5de140d7c2bfeb38166ebb79f1af9a3
sneakyPeat <david.ducluzeaud@live.fr>
2018-12-23 21:24:21 +0100

    remove rest-auth urls

--- allergen_project/urls.py
+++ allergen_project/urls.py
@@ -19,18 +19,10 @@ from django.urls import path, include, re_path
 
 from allergen import views
 
-from rest_auth.registration.views import VerifyEmailView, RegisterView
-
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include(('allergen.urls', 'allergen'), namespace='api')),
-    path('rest-auth/', include('rest_auth.urls')),
-    path('rest-auth/registration/', include('rest_auth.registration.urls')),
-    re_path(r'^account-confirm-email/', VerifyEmailView.as_view(),
-            name='account_email_verification_sent'),
-    re_path(r'^account-confirm-email/(?P<key>[-:\w]+)/$', VerifyEmailView.as_view(),
-            name='account_confirm_email'),
+    path('api/v1/', include(('allergen.urls', 'allergen'), namespace='api')),
 ]
 
 if settings.DEBUG:
