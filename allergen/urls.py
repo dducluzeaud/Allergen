092ab73fa2433732d57601af08595e6db9c8e5d8
sneakyPeat <david.ducluzeaud@live.fr>
2018-12-24 17:17:10 +0100

    black formatting

--- allergen/urls.py
+++ allergen/urls.py
@@ -8,15 +8,16 @@ from rest_framework_jwt.views import obtain_jwt_token, refresh_jwt_token
 from . import views
 
 router = routers.DefaultRouter()
-router.register(r'product', views.ProductViewSet, base_name='product')
-router.register(r'category', views.CategoryViewSet, base_name='category')
-router.register(r'ingredient', views.IngredientViewSet, base_name='ingredient')
-router.register(r'allergen', views.AllergenViewSet, base_name='allergen')
-router.register(r'additive', views.AdditiveViewSet, base_name='additive')
-router.register(r'vitamin', views.VitaminViewSet, base_name='vitamin')
-router.register(r'nutriment', views.NutrimentViewSet, base_name='nutriment')
+router.register(r"product", views.ProductViewSet, base_name="product")
+router.register(r"category", views.CategoryViewSet, base_name="category")
+router.register(r"ingredient", views.IngredientViewSet, base_name="ingredient")
+router.register(r"allergen", views.AllergenViewSet, base_name="allergen")
+router.register(r"additive", views.AdditiveViewSet, base_name="additive")
+router.register(r"vitamin", views.VitaminViewSet, base_name="vitamin")
+router.register(r"nutriment", views.NutrimentViewSet, base_name="nutriment")
+router.register(r"user", views.UserViewSet, base_name="user")
 
-schema_view = get_swagger_view(title='Allergen API')
+schema_view = get_swagger_view(title="Allergen API")
 
 urlpatterns = [
     path('', include(router.urls)),
