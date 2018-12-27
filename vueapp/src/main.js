c1a1f2f2a5f7c797ceb7814e8dcda2995e0bbd6e
sneakyPeat <david.ducluzeaud@live.fr>
2018-12-27 15:58:47 +0100

    token header

--- vueapp/src/main.js
+++ vueapp/src/main.js
@@ -2,22 +2,29 @@ import Vue from 'vue'
 import App from '@/App.vue'
 import router from '@/router'
 import store from '@/store'
-
+import axios from 'axios'
 
 import Buefy from 'buefy'
 import 'buefy/dist/buefy.css'
+import '@/assets/css/style.css'
 
 Vue.use(Buefy)
 Vue.config.productionTip = false
 
-Vue.filter('capitalize', function (value) {
+Vue.filter('capitalize', function(value) {
   if (!value) return ''
   value = value.toString()
   return value.charAt(0).toUpperCase() + value.slice(1)
 })
 
+const token = localStorage.getItem('user-token')
+
+if (token) {
+  axios.defaults.headers.common['Authorization'] = token
+}
+
 new Vue({
   router,
-  store, 
+  store,
   render: h => h(App)
 }).$mount('#app')
