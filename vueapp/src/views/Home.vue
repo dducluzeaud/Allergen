dd3f1dfa0af0aef8e870040b87120fbf95e1c58e
sneakyPeat <david.ducluzeaud@live.fr>
2018-12-28 18:51:47 +0100

    change home

--- vueapp/src/views/Home.vue
+++ vueapp/src/views/Home.vue
@@ -1,43 +1,46 @@
 <template>
-    <div class="hero-body">
-        <div class="container has-text-centered">
-            <div class="columns is-vcentered">
-                <div class="column is-5">
-                    <figure class="image is-4by3">
-                        <img src="https://picsum.photos/800/600/?random" alt="Description">
-                    </figure>
-                </div>
-                <div class="column is-6 is-offset-1">
-                    <h1 class="title is-2">
-                        Superhero Scaffolding
-                    </h1>
-                    <h2 class="subtitle is-4">
-                        Let this cover page describe a product or service.
-                    </h2>
-                    <br>
-                    <p class="has-text-centered">
-                        <a class="button is-medium is-info is-outlined">Learn more</a>
-                    </p>
-                </div>
-            </div>
+  <div class="hero-body">
+    <div class="container has-text-centered">
+      <div class="frame">
+        <p class="title">Débarrassez vous de vos allergies alimentaires !</p>
+        <p class="subtitle">Ou trouvez simplement un moyen de mieux manger ! 😁</p>
+        <div class="column is-three-fifths is-offset-one-fifth">
+          <input
+            class="input is-large is-rounded"
+            type="text"
+            placeholder="Chercher un aliment... "
+          >
         </div>
+      </div>
     </div>
+  </div>
 </template>
 
 <script>
-export default {
-    
-}
+export default {}
 </script>
 
 
-<style type="text/css">
-    html,
-    body {
-        font-family: 'Open Sans';
-        }
-    img {
-        padding: 5px;
-        border: 1px solid #ccc;
-        }
+<style type="text/css" scoped>
+.title {
+  font-size: 75px;
+  color: #255219;
+  font-weight: bolder;
+  z-index: 12456789098765432;
+}
+
+.subtitle {
+  font-size: 30px;
+  font-weight: bold;
+  opacity: 2;
+  color: #255219;
+}
+
+.hero-body {
+  background-image: url('../assets/img/home.png');
+  background-position: center center;
+  background-repeat: no-repeat;
+  background-attachment: fixed;
+  background-size: cover;
+}
 </style>
