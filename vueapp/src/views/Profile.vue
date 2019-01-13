fe5afa0ec2560faf9e11a15d89a37c6dcf7708d7
sneakyPeat <david.ducluzeaud@live.fr>
2019-01-13 22:28:14 +0100

    decode jwt

--- vueapp/src/views/Profile.vue
+++ vueapp/src/views/Profile.vue
@@ -1,13 +1,25 @@
 <template>
   <div class="profile">
-    <h1 class="title">{{authUser}}</h1>
+    <h1 class="title">{{user}}</h1>
   </div>
 </template>
 
 <script>
 import { mapGetters } from 'vuex'
+import jwt_decode from 'jwt-decode'
 
 export default {
+  data() {
+    return {
+      user: this.user,
+      email: this.email
+    }
+  },
+  created() {
+    const payload = jwt_decode(this.$store.state.jwt)
+    this.user = payload.username
+    this.email = payload.email
+  },
   computed: {
     ...mapGetters(['authUser'])
   }
