a7bb870716903e2265da2ace70e407e90f6a1db1
sneakyPeat <david.ducluzeaud@live.fr>
2019-01-15 22:38:44 +0100

    add base url for axios

--- vueapp/src/main.js
+++ vueapp/src/main.js
@@ -11,7 +11,7 @@ import '@/assets/css/style.css'
 Vue.use(Buefy)
 Vue.config.productionTip = false
 
-Vue.filter('capitalize', value => {
+Vue.filter('capitalize', (value) => {
   if (!value) return ''
   value = value.toString().toLowerCase()
   if (value.length > 43) {
@@ -25,6 +25,8 @@ Vue.filter('capitalize', value => {
 
 const token = localStorage.getItem('user-token')
 
+axios.defaults.baseURL = process.env.VUE_APP_ROOT_API
+
 if (token) {
   axios.defaults.headers.common['Authorization'] = token
 }
@@ -32,5 +34,5 @@ if (token) {
 new Vue({
   router,
   store,
-  render: h => h(App)
+  render: (h) => h(App),
 }).$mount('#app')
