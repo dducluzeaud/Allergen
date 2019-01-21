959eb6b33919affe9576eae1b9e4e323b71d94ac
sneakyPeat <david.ducluzeaud@live.fr>
2019-01-21 23:29:56 +0100

    filter emojize

--- vueapp/src/main.js
+++ vueapp/src/main.js
@@ -11,7 +11,7 @@ import '@/assets/css/style.css'
 Vue.use(Buefy)
 Vue.config.productionTip = false
 
-Vue.filter('capitalize', (value) => {
+Vue.filter('capitalize', value => {
   if (!value) return ''
   value = value.toString().toLowerCase()
   if (value.length > 43) {
@@ -21,6 +21,25 @@ Vue.filter('capitalize', (value) => {
     value = `${value} ...`
   }
   return value.charAt(0).toUpperCase() + value.slice(1)
+})
+
+Vue.filter('emojizeRisk', risk => {
+  switch (risk) {
+    case 0:
+      return '✅'
+    case 1:
+      return '⚠️'
+    case 2:
+      return '❓'
+    case 3:
+      return '⁉️'
+    case 4:
+      return '⛔️'
+  }
+})
+
+Vue.filter('normalize', value => {
+  return parseInt(value / 1000, 10)
 })
 
 const token = localStorage.getItem('user-token')
@@ -34,5 +53,5 @@ if (token) {
 new Vue({
   router,
   store,
-  render: (h) => h(App),
+  render: h => h(App)
 }).$mount('#app')
