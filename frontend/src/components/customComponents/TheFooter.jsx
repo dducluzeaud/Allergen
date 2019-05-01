ad74efa2e2a98c64539ff53a1adf2f5e6a57941a
sneakyPeat <david.ducluzeaud@live.fr>
2019-05-01 22:32:49 +0200

    using material

---
+++ frontend/src/components/customComponents/TheFooter.jsx
@@ -0,0 +1,11 @@
+// import React from 'react';
+
+// import { Link } from 'react-router-dom';
+
+// const TheFooter = () => (
+//   <>
+//     <p>TOLO</p>
+//   </>
+// );
+
+// export default TheFooter;
---
+++ frontend/src/routes.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Route, HashRouter, Switch } from 'react-router-dom';
+
+import Home from './components/views/Home';
+import AdditifTable from './components/views/AdditifTable';
+
+export default props => (
+  <HashRouter>
+    <Switch>
+      <Route exact path="/" component={Home} />
+      <Route exact path="/additifs" component={AdditifTable} />
+    </Switch>
+  </HashRouter>
+);
