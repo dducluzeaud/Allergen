7a7c15462d545f93576f310f593b9ee52ddbf13b
dducluzeaud <david.ducluzeaud@live.fr>
2019-07-10 11:39:16 +0200

    get rid of api/v1

--- api/allergen_project/urls.py
+++ api/allergen_project/urls.py
@@ -20,7 +20,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("api/v1/", include(("allergen.urls", "allergen"), namespace="api")),
 ]
 
 if settings.DEBUG:
