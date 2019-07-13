ed348afd64ca06cdf865e0a59d63c3031bc29856
dducluzeaud <david.ducluzeaud@live.fr>
2019-07-13 14:51:18 +0200

    api

--- api/allergen_project/urls.py
+++ api/allergen_project/urls.py
@@ -20,6 +20,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("api/", include(("allergen.urls", "allergen"), namespace="api")),
 ]
 
 if settings.DEBUG:
