99fb4c7bece321866a8ce5e48c1642dc3aca636d
David Ducluzeaud <david.ducluzeaud@live.fr>
2019-07-13 14:42:48 +0200

    fix Travis CI (#12)
    * update npm packages
    
    * fix translation tool
    
    * fix travis CI :rocket:
    
    * change Api -> api
    
    * codecov
    
    * rm useles urls

--- api/allergen_project/urls.py
+++ api/allergen_project/urls.py
@@ -20,7 +20,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("api/v1/", include(("allergen.urls", "allergen"), namespace="api")),
 ]
 
 if settings.DEBUG:
@@ -28,5 +27,4 @@ if settings.DEBUG:
 
     urlpatterns = [
         path("__debug__/", include(debug_toolbar.urls)),
-        path("", include(("allergen.urls", "allergen"), namespace="allergen")),
     ] + urlpatterns
